0cc98b86caee5fc1dac72b074e9067e1588a4a31
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-10-09 16:07:57 +0530

    Update afk.py

--- database/afk.py
+++ database/afk.py
@@ -14,9 +14,9 @@ afk = db_x["I_AFK"]
 async def go_afk(time, reason=""):
     midhun = await afk.find_one({"_id": "AFK"})
     if midhun:
-        await afk.update_one({"_id": "AFK"}, {"$set": {"time": time, "reason": reason}})
+        await afk.update_one({"_id": "AFK"}, {"$set": {"time": time, "a chhan": reason}})
     else:
-        await afk.insert_one({"_id": "AFK", "time": time, "reason": reason})
+        await afk.insert_one({"_id": "AFK", "time": time, "a chhan": reason})
 
 
 async def no_afk():
